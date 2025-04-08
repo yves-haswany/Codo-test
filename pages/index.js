808cd3398e3d86caf0038430fbdd3ad0f75a0fd3
yves_haswany <yves2k611@gmail.com>
2025-04-08 10:56:32 +0300

    Add index.js

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+// pages/index.js
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <html lang="en">
+      <head>
+        <title>Codo</title>
+        <link rel="stylesheet" href="styles.css" />
+      </head>
+      <body>
+        <header>
+          <nav>
+            <ul>
+              <li><a href="index.html">Home</a></li>
+              <li><a href="AboutUs.html">About us</a></li>
+              <li><a href="ContactUs.html">Contact us</a></li>
+            </ul>
+          </nav>
+        </header>
+        <p>Welcome to Codo, your friendly platform to handle
+           invoicing and billing for charging your electric
+           vehicles.
+        </p>
+      </body>
+    </html>
+  );
+};
+
+export default Home;
